d8573ba23057704ea018675284afd3ce386ba45f
Kok Yong <KokYong@Koks-MacBook-Pro.local>
2017-02-17 12:39:19 +0800

    uniform

---
+++ Insta.Gram/PostPictureCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  PostPictureCollectionViewCell.swift
+//  Insta.Gram
+//
+//  Created by Kok Yong on 16/02/2017.
+//  Copyright © 2017 Kok Yong. All rights reserved.
+//
+
+import UIKit
+
+class PostPictureCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+}
